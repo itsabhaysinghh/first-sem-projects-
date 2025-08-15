de1a481a2a4e01d6145e18b397638cf88c202de9
Abhay Singh <itsabhaysingh63@gmail.com>
2025-08-15 21:59:22 +0530

    Create GuessTheNumber.py

---
+++ GuessTheNumber.py
@@ -0,0 +1,22 @@
+import random as random
+
+
+class GuessTheNumber:
+    def guess(self,a,b):
+        if User_input == computer:
+            return "You Won!"
+        
+        else:
+            return "You Lost!"
+ 
+        
+
+
+
+User_input = int(input("Enter your guess number beetween 1,100: ")) 
+computer = random.randint(1,100)
+game = GuessTheNumber()
+result = game.guess(User_input,computer)
+print(result)
+
+
